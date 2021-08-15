f8aacc2bc663b154a5d819dcb370c804ffe8e3e4
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-08-15 22:34:05 +0530

    backend first commit

---
+++ Backend/utils/connection.js
@@ -0,0 +1,25 @@
+const {MongoClient} = require('mongodb')
+require('dotenv').config()
+
+const URI =process.env.URI
+
+
+const client = new MongoClient(URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+client.connect(err=>{
+
+    if(err){
+        
+        if(err){
+            console.log(err)
+        }
+
+    }
+
+    console.log("db connected")
+
+});
+
