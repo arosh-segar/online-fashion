5c239e426216072eeaf1552c4076d99daccb3563
arosh-segar <arosh.segar@gmail.com>
2021-08-17 23:43:33 +0530

    Add stock committed

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,25 @@
-const Express = require('express');
-const cors = require('cors')
+const Express = require("express");
+const cors = require("cors");
 
-const SupplierRoutes = require('./routes/supplier.route');
-const InventoryRoutes = require('./routes/inventory.route');
-const DeliveryRoutes = require('./routes/delivery.route');
+//const SupplierRoutes = require("./routes/supplier.route");
+const InventoryRoutes = require("./routes/inventory.route");
+//const DeliveryRoutes = require("./routes/delivery.route");
 
-require('./utils/connection')
+require("./utils/connection");
 
+const app = new Express();
 
-const app = new Express()
+app.use(cors());
+app.use(Express.json());
 
-app.use(cors())
-app.use(Express.json())
+//app.use("/supplier", SupplierRoutes);
+app.use("/inventory", InventoryRoutes);
+//app.use("/delivery", DeliveryRoutes);
 
-app.use("/supplier",SupplierRoutes)
-app.use("/inventory",InventoryRoutes)
-app.use("/delivery",DeliveryRoutes)
-
-
-app.listen(5000,err=>{
-
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('Backend Service is running on port 5000...');
-
-})
-
-
-
-
+app.listen(5000, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("Backend Service is running on port 5000...");
+});
--- Backend/utils/connection.js
+++ Backend/utils/connection.js
@@ -1,25 +1,21 @@
-const {MongoClient} = require('mongodb')
-require('dotenv').config()
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
 
-const URI =process.env.URI
-
+const URI = process.env.URI;
 
 const client = new MongoClient(URI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-client.connect(err=>{
-
-    if(err){
-        
-        if(err){
-            console.log(err)
-        }
-
+client.connect((err) => {
+  if (err) {
+    if (err) {
+      console.log(err);
     }
+  }
 
-    console.log("db connected")
-
+  console.log("db connected");
 });
 
+module.exports = client;
