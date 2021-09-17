cb65ac18566bab847ee0ca64e5805922bb05ee52
Yadhursheeka <it19186948@my.sliit.lk>
2021-09-17 12:08:40 +0530

    commit customer registration

--- Backend/dal/customer.dao.js
+++ Backend/dal/customer.dao.js
@@ -1,4 +1,5 @@
 let order = null;
+let customer = null;
 let stock = null;
 
 const ObjectID = require("mongodb").ObjectId;
@@ -8,10 +9,21 @@ setTimeout(async () => {
     .db("OnlineFashionDB")
     .collection("order");
 
+  customer = await require("../utils/connection")
+    .db("OnlineFashionDB")
+    .collection("customer");
+
   stock = require("../utils/connection")
     .db("OnlineFashionDB")
     .collection("stock");
 }, 3000);
+
+// ---------------------- CUSTOMER COLLECTION ----------------------------------
+// Customer Registration
+const createCustomer = async (customerObject) => {
+  const result = await order.insertOne(customerObject);
+  return result;
+};
 
 // ----------------------------------- ORDER COLLECTION ------------------------
 
@@ -27,6 +39,30 @@ const getAllOrders = async (email) => {
   return results.toArray();
 };
 
+// Edit Order - customers edit the quantity of products they have ordered
+const editOrder = async (id, editOrderObject) => {
+  // return await upload.updateOne({"_id":ObjectID(id)}, {$set: {status:status, reviewerID:reviewerID}})
+
+  return await order.updateOne(
+    { _id: ObjectID(id) },
+    {
+      $set: {
+        purchaseDate: editOrderObject.purchaseDate,
+        products: editOrderObject.products,
+        totalBillAmount: editOrderObject.totalBillAmount,
+        status: editOrderObject.status,
+        customerEmail: editOrderObject.customerEmail,
+        deliveryAddress: editOrderObject.deliveryAddress,
+      },
+    }
+  );
+};
+
+// DELETE ORDER PLACED BY CUSTOMER
+const deleteOrder = async (id) => {
+  return await order.deleteOne({ _id: ObjectID(id) });
+};
+
 // Retrieve Products to purchase
 const getAllStocks = async () => {
   const results = await stock.find({});
@@ -34,7 +70,10 @@ const getAllStocks = async () => {
 };
 
 module.exports = {
+  createCustomer,
   createOrder,
+  editOrder,
+  deleteOrder,
   getAllOrders,
   getAllStocks,
 };
